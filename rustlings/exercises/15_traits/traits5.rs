7a4b544e579d22405edbd7e6ae5a8e2aa9b55826
markgreene74 <markgreene74@users.noreply.github.com>
2024-09-12 13:20:03 +0000

    solutions for exercises/15_traits

--- rustlings/exercises/15_traits/traits5.rs
+++ rustlings/exercises/15_traits/traits5.rs
@@ -19,7 +19,12 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// Alternative synthax:
+// fn some_func<T>(item: T) -> bool
+// where
+//     T: SomeTrait + OtherTrait,
+// {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
