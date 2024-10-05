d4bec8c087cf5241f6a3712e5b0724ddb2d5d341
markgreene74 <markgreene74@users.noreply.github.com>
2024-10-05 16:45:56 +0000

    solutions for exercises/21_macros

--- rustlings/exercises/21_macros/macros2.rs
+++ rustlings/exercises/21_macros/macros2.rs
@@ -3,6 +3,10 @@ fn main() {
 }
 
 // TODO: Fix the compiler error by moving the whole definition of this macro.
+// Move the macro up the file, before main(). Alternatively, use macro_export to
+// declare the macro in the root scope, see
+// https://doc.rust-lang.org/reference/macros-by-example.html#path-based-scope
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
--- rustlings/exercises/21_macros/macros3.rs
+++ rustlings/exercises/21_macros/macros3.rs
@@ -1,6 +1,10 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+// Solution 1: add macro_use here
+#[macro_use]
 mod macros {
+    // Solution 2: add macro_export here
+    // #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
